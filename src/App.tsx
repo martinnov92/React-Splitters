c9a7c2150b12e2eb7f5ec404863d9593b47478af
Martin Novák <martinnovak@outlook.com>
2017-03-01 16:24:31 +0100

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -37,10 +37,30 @@ class App extends React.Component<null, AppState> {
                 dispatchResize={true}
                 postPoned={true}
               >
-                <div />
-                <div />
+              <div className="placeholder _1">
+                <span>1</span>
+                <p>postponed</p>
+                <p>primary</p>
+              </div>
+              <Splitter
+                  position="vertical"
+                  primaryPaneMaxWidth="80%"
+                  primaryPaneMinWidth={0}
+                  primaryPaneWidth="400px"
+                  postPoned={false}
+              >    
+                  <div className="placeholder _2">
+                    <span>2</span>
+                    <p>normal resize</p>
+                    <p>primary</p>
+                  </div>
+                  <div className="placeholder _3"><span>3</span></div>
+              </Splitter> 
             </Splitter>
-            <div />
+            <div className="placeholder _4">
+              <span>4</span>
+              <p>normal resize</p>  
+            </div>
           </Splitter>         
         </div>
       </div>
