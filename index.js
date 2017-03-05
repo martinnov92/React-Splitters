0f315804d5d111db0f3891bda8bc2f1ffcdc5bff
Martin Novák <martinnovak@outlook.com>
2017-03-05 09:09:55 +0100

    index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-var Splitter = require("./lib/Splitter");
-
-module.exports = Splitter;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Splitter_1 = require("./Splitter");
+exports.default = Splitter_1.Splitter;
