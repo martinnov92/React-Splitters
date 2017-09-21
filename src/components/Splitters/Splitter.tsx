6afd868d5ff1ba19cbad7c3f8273182a074b3c80
Martin Novak <martinnovak@outlook.com>
2017-09-21 17:32:11 +0200

    ...

--- src/components/Splitters/Splitter.tsx
+++ src/components/Splitters/Splitter.tsx
@@ -35,13 +35,15 @@ export class Splitter extends React.Component<SplitterProps, SplitterState> {
 
     constructor() {
         super();
-        this.handleMouseDown = this.handleMouseDown.bind(this);
-        this.handleMouseUp = this.handleMouseUp.bind(this);
-        this.handleMouseMove = this.handleMouseMove.bind(this);
-        this.getSize = this.getSize.bind(this);
+
         this.state = {
             isDragging: false
         };
+
+        this.getSize = this.getSize.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
     }
 
     getSize(cX?: Number | any, cY?: Number | any) {
@@ -51,9 +53,11 @@ export class Splitter extends React.Component<SplitterProps, SplitterState> {
         let maxMousePosition;
         let nodeWrapperSize;
         let primaryPaneOffset;
+
         let wrapper = ReactDOM.findDOMNode(this.paneWrapper).getBoundingClientRect();
         let primaryPane = ReactDOM.findDOMNode(this.panePrimary).getBoundingClientRect();
         let handleBarSize = ReactDOM.findDOMNode(this.handlebar).getBoundingClientRect();
+
         const posInHandleBar = this.props.position === 'vertical' 
             ? handleBarSize.left - cX
             : handleBarSize.top - cY;
@@ -65,6 +69,7 @@ export class Splitter extends React.Component<SplitterProps, SplitterState> {
             // split the maxWidth/maxHeight string to string and numbers
             let maxWidthStr = this.props.primaryPaneMaxWidth.match(regEx)[0].toLowerCase();
             let maxWidthNum = parseFloat(this.props.primaryPaneMaxWidth.split(regEx)[0]);
+
             nodeWrapperSize = wrapper.width;
             primaryPaneOffset = primaryPane.left;
 
